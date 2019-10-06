681a87c071655f9ec36b16b3f2238c2dbb343519
Alena <mouse.programmouse@gmail.com>
2019-10-05 17:01:29 -0700

    add an active item to the top menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react'
-import { Route } from 'react-router-dom'
+import { Route, withRouter} from 'react-router-dom'
 import MainMenu from './MainMenu'
 import './App.css'
 
 class App extends Component {
   state = {
-    activeLink : '/',
     menuItems: [{linkTitle: 'Home', link: '/'},
                 {linkTitle: 'email me', link: '/get-in-touch'},
                 {linkTitle: 'resume', link: '/resume'},
@@ -14,28 +13,28 @@ class App extends Component {
 
   render () {
     return (
-      <div className = 'app'>
+      <div className = "app">
         <Route exact path = '/' render = { () => (
-          <main className = 'main-page'>
-          <MainMenu activeLink = { this.state.activeLink } menuItems = { this.state.menuItems }/>
+          <main className = "main-page">
+          <MainMenu activeLink = {this.props.location.pathname} menuItems = { this.state.menuItems }/>
           Main page
           </main>
         )} />
         <Route exact path = '/get-in-touch' render = { () => (
-          <main className = 'get-in-touch'>
-          <MainMenu activeLink = { this.state.activeLink } menuItems = { this.state.menuItems }/>
+          <main className = "get-in-touch">
+          <MainMenu activeLink = {this.props.location.pathname} menuItems = { this.state.menuItems }/>
           Email me to '''mouse.programmouse@gmail.com'''
           </main>
         )} />
         <Route exact path = '/resume' render = { () => (
-          <main className = 'resume-page'>
-          <MainMenu activeLink = { this.state.activeLink } menuItems = { this.state.menuItems }/>
+          <main className = "resume-page">
+          <MainMenu activeLink = {this.props.location.pathname} menuItems = { this.state.menuItems }/>
           Resume page
           </main>
         )} />
         <Route exact path = '/portfolio' render = { () => (
-          <main className = 'portfolio-page'>
-          <MainMenu activeLink = { this.state.activeLink } menuItems = { this.state.menuItems }/>
+          <main className = "portfolio-page">
+          <MainMenu activeLink = {this.props.location.pathname} menuItems = { this.state.menuItems }/>
           My portfolio
           </main>
         )} />
@@ -44,4 +43,4 @@ class App extends Component {
   }
 }
 
-export default App
+export default withRouter(App)
